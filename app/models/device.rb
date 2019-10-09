7ad825f9b218a9cc468af5b118e3b745712ae72e
Nico De Witte <nico.dewitte@outlook.com>
2019-10-09 18:09:26 +0200

    Fix mac address format to also work with single nibble

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,7 @@
 class Device < ApplicationRecord
   belongs_to :device, optional: true
 
-  validates :mac, format: { with: /\A([0-9a-fA-F]{2}[:-]){5}[0-9a-fA-F]{2}\z/i },
+  validates :mac, format: { with: /\A([0-9A-Fa-f]{1,2}[-:]){5}([0-9A-Fa-f]{1,2})\z/ },
     presence: true, uniqueness: true
 
   before_save :downcase_mac
