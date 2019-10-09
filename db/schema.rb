d91033047c1f3cb729a355296183719890d2577e
Nico De Witte <nico.dewitte@outlook.com>
2019-10-09 17:55:26 +0200

    Add scaffold of Device resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_155100) do
+ActiveRecord::Schema.define(version: 2019_10_09_155358) do
+
+  create_table "devices", force: :cascade do |t|
+    t.string "name"
+    t.string "mac"
+    t.string "ip"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
