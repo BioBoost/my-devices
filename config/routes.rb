ac43069cb6cd61603ba2be854beaf058e66bb583
Nico De Witte <nico.dewitte@outlook.com>
2019-10-10 13:02:50 +0200

    Setup routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :devices
-  resources :users
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#home"
   get '/api/docs', to: 'static_pages#api_docs', as: 'api_docs'
   post '/updates', to: 'updates#create', as: 'update'
+
+  Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      sessions: 'users/sessions'
+    }
+  end
 end
